9649903726d2c8d0afaf546c035d983df292da84
Matt Mazon <matt.m.mazon@gmail.com>
2022-07-14 18:48:15 -0700

    Added movement to the ship

--- settings.py
+++ settings.py
@@ -6,4 +6,4 @@ class Settings:
         #Screen settings 
         self.screen_width = 1920
         self.screen_height = 1080
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (0, 0, 0)
